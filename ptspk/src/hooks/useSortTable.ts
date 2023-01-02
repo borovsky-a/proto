8a972e293704f4530d8db57c4207a43cdee8189c
borovsky-a <95590354+borovsky-a@users.noreply.github.com>
2023-01-02 22:33:19 +0300

    фикс бага с сортировкой

--- ptspk/src/hooks/useSortTable.ts
+++ ptspk/src/hooks/useSortTable.ts
@@ -1,66 +1,70 @@
-import  { useEffect, useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import _ from 'lodash';
 
 export type SortTableDirection = 'ascending' | 'descending' | undefined;
 export const CHANGE_SORT = 'CHANGE_SORT';
 export const CHANGE_DATA = 'CHANGE_DATA';
 type SortTableArgs = {
-    column: string | null,
-    data: any[],
-    direction: SortTableDirection,
+   column: string | null,
+   data: any[],
+   direction: SortTableDirection,
 }
-const sortableReducer = (state: any, action: any) => {  
-    switch (action.type) {
-       case CHANGE_SORT:
-          if (state.column === action.column) {
-             return {
-                ...state,
-                data: state.data.slice().reverse(),
-                direction:
-                   state.direction === 'ascending' ? 'descending' : 'ascending',
-             }
-          }
-          return {
-            ...state,
-             column: action.column,
-             data: _.sortBy(state.data, [action.column]),
-             direction: 'ascending',
-          }
-          case CHANGE_DATA :
-            {
-               return  {...state, data: action.data};
+const sortableReducer = (state: any, action: any) => {
+   switch (action.type) {
+      case CHANGE_SORT:
+         if (state.column === action.column) {
+            return {
+               ...state,
+               data: state.data.slice().reverse(),
+               direction:
+                  state.direction === 'ascending' ? 'descending' : 'ascending',
             }
-       default:
-          throw new Error()
-    }
- }
- 
+         }
+         return {
+            column: action.column,
+            data: _.sortBy(state.data, [action.column]),
+            direction: 'ascending',
+         }
+      case CHANGE_DATA:
+         {
+            if(!state.column){
+               return { ...state, data: state.data };
+            }
+            const asc = state.direction === 'ascending' || !state.direction;
+            const data =  asc ?  _.sortBy(action.data, [state.column]) : _.orderBy(action.data, state.column, 'desc');
+            return { ...state, data: data };
+         }
+      default:
+         throw new Error()
+   }
+}
+
 export function useSortTable<T>(items: any[]) {
 
-    const initState: SortTableArgs ={
-        column: null,
-        data: items,
-        direction: undefined
-    }
-    const [state, dispatch] = useReducer(sortableReducer, initState);
+   const initState: SortTableArgs = {
+      column: null,
+      data: items,
+      direction: undefined
+   }
+   const [state, dispatch] = useReducer(sortableReducer, initState);
 
-    const onSort = (column: string) : SortTableDirection =>{
-       return state.column === column ? state.direction : undefined;
-    }
+   const onSort = (column: string): SortTableDirection => {
+      return state.column === column ? state.direction : undefined;
+   }
 
-    const onClick = (column: string) => {
-      return ()=>  dispatch({ type: CHANGE_SORT, column: column })
-    }
+   const onClick = (column: string) => {
+      return () => dispatch({ type: CHANGE_SORT, column: column })
+   }
 
-    useEffect(()=>{
+   useEffect(() => {
       dispatch({ type: CHANGE_DATA, data: items });
-    },[items]);
+   }, [items]);
 
-    return {
-        column: state.column,
-        data: state.data as T[],
-        direction: state.direction,
-        onSort,
-        onClick
-    }
+   return {
+      column: state.column,
+      data: state.data as T[],
+      direction: state.direction,
+      onSort,
+      onClick
+   }
 }
